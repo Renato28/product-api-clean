d728f608b4e6a64e77a9cd46deaf879d869cc6ce
Renato28 <renatonobrega87@gmail.com>
2024-12-19 20:00:17 -0300

    implementando a classe de RegraDeNegocioException

--- src/main/java/io/github/renatonobrega/produto_api/core/exceptions/RegraDeNegocioException.java
+++ src/main/java/io/github/renatonobrega/produto_api/core/exceptions/RegraDeNegocioException.java
@@ -1,4 +1,8 @@
 package io.github.renatonobrega.produto_api.core.exceptions;
 
-public class RegraDeNegocioException {
+public class RegraDeNegocioException extends RuntimeException {
+
+    public RegraDeNegocioException(String mensagem) {
+        super(mensagem);
+    }
 }
